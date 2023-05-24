59b4662a80c33ba45664dff4ec10a9f0ef785169
Yang Hyemin <asitwas729@gmail.com>
2023-05-24 16:28:10 +0900

    Initial commit

---
+++ src/main/java/com/example/ldw_springaws/LdwSpringAwsApplication.java
@@ -0,0 +1,13 @@
+package com.example.ldw_springaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LdwSpringAwsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LdwSpringAwsApplication.class, args);
+  }
+
+}
